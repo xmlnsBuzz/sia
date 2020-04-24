8b7ce465514941f094d50602b31d073d71491c56
SIA <sia.channel.code@gmail.com>
2020-04-24 17:53:48 +0900

    Add picture for font-style

--- pages/docs/css/font/FontStyle_C.js
+++ pages/docs/css/font/FontStyle_C.js
@@ -4,18 +4,21 @@ class FontStyle_C extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <section id="">
+        <section id="page-top">
           <div className="align-center">
             <h1>font-style Property</h1>
           </div>
           <div className="normal-wrap">
             <hr />
-            <h2></h2>
+            <h2 id="Styling">Styling</h2>
+            <p className="lead">'스타일링'이란 말이 있다. 흔히 자기 나름대로 머리에 멋을 부리는 것을 '헤어스타일링'이라 하는데, 생긴대로 normal하게 다니는 사람이 있는가 하면 빠마를 하는 사람, 무스로 스타일링하는 사람 등 뭔가 변화를 시도하는 사람도 있다. 아마도 인간이 가진 특성중 하나인 '변덕' 때문이 아닐까 한다. 그래서 사람이 한 가지 일을 꾸준히 한다는게 여간 어려운 일이 아니라는 것은 모두 잘 알 것이다.</p>
+            <h2 id="FontStyle">font-style</h2>
             <p className="lead"></p>
             <div className="normal-wrap">
               <h3></h3>
               <p class="lead"></p>
             </div>
+            
           </div>
           <h2></h2>
           <p className="lead"></p>
--- pages/docs/css/font/FontStyle_N.js
+++ pages/docs/css/font/FontStyle_N.js
@@ -6,17 +6,17 @@ class FontStyle_N extends React.Component {
       <React.Fragment>
         <div className="container text-center">
           <div className="bookmark-header">
-             <a className="navbar-brand js-scroll-trigger" href="#page-top">
+             <a className="navbar-brand js-scroll-trigger" href="#scrollSpy">
                font-style
              </a>
            </div>
           <nav className="collapse navbar-collapse text-left overflow-scroll">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#about"></a>
+                <a className="nav-link js-scroll-trigger" href="#Styling">Styling</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#services"></a>
+                <a className="nav-link js-scroll-trigger" href="#FontStyle">font-style</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link js-scroll-trigger" href="#contact"></a>
